66b167a6d0d6b0e3f3d08b0a3a4e41f8ba9ab467
ALHAN710 <alhadoumpascal@gmail.com>
2020-04-03 17:04:27 +0100

    MAJ Formulaire User

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -2,28 +2,91 @@
 
 namespace App\Form;
 
-use App\Entity\User;
+//use App\Entity\User;
 use App\Entity\User1;
-use Symfony\Component\Form\AbstractType;
+use App\Form\ApplicationType;
+//use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
-class AccountType extends AbstractType
+class AccountType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //->add('firstName')
-            //->add('lastName')
-            ->add('enterpriseName')
-            ->add('email')
-            ->add('avatar')
-            //->add('picture')
-            //->add('hash')
-            //->add('introduction')
-            //->add('description')
-            //->add('slug')
-        ;
+            ->add(
+                'enterpriseName',
+                TextType::class,
+                $this->getConfiguration("Nom de l'entreprise", "Nom de l'entreprise")
+            )
+
+            ->add(
+                'email',
+                EmailType::class,
+                $this->getConfiguration("Email", "Email de l'entreprise")
+            )
+            ->add(
+                'avatar',
+                FileType::class,
+                $this->getConfiguration(
+                    "Avatar Utilisateur (IMG file)",
+                    "",
+                    [
+                        // unmapped means that this field is not associated to any entity property
+                        'mapped' => false,
+
+                        // make it optional so you don't have to re-upload the IMG file
+                        // every time you edit the Product details
+                        'required' => false,
+
+                        // unmapped fields can't define their validation using annotations
+                        // in the associated entity, so you can use the PHP constraint classes
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '1024k',
+                                'mimeTypes' => [
+                                    'image/png',
+                                    'image/jpeg',
+                                ],
+                                'mimeTypesMessage' => 'Please upload a valid Image format(jpeg, png)',
+                            ])
+                        ],
+                    ]
+                )
+            )
+            ->add(
+                'enterpriseLogo',
+                FileType::class,
+                $this->getConfiguration(
+                    "Logo Entreprise (IMG file)",
+                    "",
+                    [
+                        // unmapped means that this field is not associated to any entity property
+                        'mapped' => false,
+
+                        // make it optional so you don't have to re-upload the IMG file
+                        // every time you edit the Product details
+                        'required' => false,
+
+                        // unmapped fields can't define their validation using annotations
+                        // in the associated entity, so you can use the PHP constraint classes
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '1024k',
+                                'mimeTypes' => [
+                                    'image/png',
+                                    'image/jpeg',
+                                ],
+                                'mimeTypesMessage' => 'Please upload a valid Image format(jpeg, png)',
+                            ])
+                        ],
+                    ]
+                )
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/Registration1Type.php
+++ src/Form/Registration1Type.php
@@ -6,7 +6,9 @@ use App\Entity\Role;
 use App\Entity\User1;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -19,31 +21,83 @@ class Registration1Type extends ApplicationType
         $builder
             ->add(
                 'enterpriseName',
-                TextType::class, 
-                $this->getConfiguration("Nom de l'entreprise","Nom de l'entreprise")
+                TextType::class,
+                $this->getConfiguration("Nom de l'entreprise", "Nom de l'entreprise")
             )
-            
+
             ->add(
                 'email',
-                EmailType::class, 
-                $this->getConfiguration("Email","Email de l'entreprise")
+                EmailType::class,
+                $this->getConfiguration("Email", "Email de l'entreprise")
             )
             ->add(
                 'avatar',
-                TextType::class, 
-                $this->getConfiguration("Logo","URL du logo de l'entreprise")
+                FileType::class,
+                $this->getConfiguration(
+                    "Avatar Utilisateur (IMG file)",
+                    "",
+                    [
+                        // unmapped means that this field is not associated to any entity property
+                        'mapped' => false,
+
+                        // make it optional so you don't have to re-upload the IMG file
+                        // every time you edit the Product details
+                        'required' => false,
+
+                        // unmapped fields can't define their validation using annotations
+                        // in the associated entity, so you can use the PHP constraint classes
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '1024k',
+                                'mimeTypes' => [
+                                    'image/png',
+                                    'image/jpeg',
+                                ],
+                                'mimeTypesMessage' => 'Please upload a valid Image format(jpeg, png)',
+                            ])
+                        ],
+                    ]
+                )
+            )
+            ->add(
+                'enterpriseLogo',
+                FileType::class,
+                $this->getConfiguration(
+                    "Logo Entreprise (IMG file)",
+                    "",
+                    [
+                        // unmapped means that this field is not associated to any entity property
+                        'mapped' => false,
+
+                        // make it optional so you don't have to re-upload the IMG file
+                        // every time you edit the Product details
+                        'required' => false,
+
+                        // unmapped fields can't define their validation using annotations
+                        // in the associated entity, so you can use the PHP constraint classes
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '1024k',
+                                'mimeTypes' => [
+                                    'image/png',
+                                    'image/jpeg',
+                                ],
+                                'mimeTypesMessage' => 'Please upload a valid Image format(jpeg, png)',
+                            ])
+                        ],
+                    ]
+                )
             )
             ->add(
                 'hash',
-                PasswordType::class, 
-                $this->getConfiguration("Mot de passe","Entrer votre mot de passe")
+                PasswordType::class,
+                $this->getConfiguration("Mot de passe", "Entrer votre mot de passe")
             )
             ->add(
-                'passwordConfirm', 
+                'passwordConfirm',
                 PasswordType::class,
                 $this->getConfiguration("Confirmation de mot de passe", "Veuillez confirmer votre mot de passe")
-            )
-        ;
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
